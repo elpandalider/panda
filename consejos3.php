b160d6163e7965e43d149fe6cd7ba1ec1135fa7c
Gadiel Hernandez <Gadiel Hernandez>
2016-11-14 04:16:45 -0600

    Diseño Cons3

--- consejos3.php
+++ consejos3.php
@@ -2,6 +2,7 @@
 <html>
   <head>
     <!--Stylesheets-->
+    <link rel="stylesheet" href="css/animations.css" type="text/css">
     <link rel="stylesheet" href="node_modules\bootstrap\dist\css\bootstrap.css" media="screen" title="no title">
     <link rel="stylesheet" href="node_modules\bootstrap-material-design\dist\css\bootstrap-material-design.css" media="screen" title="no title">
     <link rel="stylesheet" href="node_modules\bootstrap-material-design\dist\css\ripples.css" media="screen" title="no title">
@@ -13,6 +14,7 @@
     <link href="//fonts.googleapis.com/css?family=Oswald&subset=latin" rel="stylesheet" type="text/css">
     <link href="//fonts.googleapis.com/css?family=Oxygen&subset=latin" rel="stylesheet" type="text/css">
     <link href="//fonts.googleapis.com/css?family=Roboto:900&substet=latin" rel="stylesheet" type="text/css">
+    <link href=";">
     <meta charset="utf-8">
     <title>PANDA</title>
   </head>
@@ -22,71 +24,111 @@
   <?php require 'nav.php'; ?>
 
     <div class="container">
-      <div class="text-center col-md-10 col-md-offset-1 titulo">
-        <div class="page-header">
+      <div class="text-center col-md-10 col-md-offset-1 titulo animatedParent">
+        <div class="page-header animated fadeInDownShort slowest">
           <h2>TDAH y gestión del tiempo</h2>
-      </div>
+        </div>
       </div>
       <div class="row">
         <div class="col-md-10 col-md-offset-1">
-          <h3 align="center"> Los niños con TDAH y sus problemas al manejar el tiempo </h3>
-          <p class="cons">
-            Los niños con TDAH tienen problemas para manejarse con el tiempo pero ¿cómo sabemos qué tan grave es esto?, con los siguientes
-            puntos podriarnos darnos una idea de la gravedad en donde se encuentran los niños.
-          </p>
-          <ul>
-            <li class="cons">¿Se trata de una dificultad con intervalos muy breves (menos de un segundo), breves (segundos o minutos), largos (horas) o muy largos (días y semanas)?</li>
-            <li class="cons">¿Es una dificultad para percibir el paso del tiempo, para medirlo o para tenerlo presente?</li>
-            <li class="cons">¿Cómo evolucionan estos problemas?</li>
+          <div class="animatedParent">
+            <h3 align="center" class="animated fadeInUpShort slowest"> Los niños con TDAH y sus problemas al manejar el tiempo </h3>
+            <p class="intro animated fadeInUpShort slowest">
+              Los niños con TDAH tienen problemas para manejarse con el tiempo pero ¿cómo sabemos qué tan grave es esto?, con los siguientes
+              puntos podriarnos darnos una idea de la gravedad en donde se encuentran los niños.
+            </p>
+            <ul class="animated fadeInUpShort slowest">
+              <li>  <h5>¿Se trata de una dificultad con intervalos muy breves (menos de un segundo), breves (segundos o minutos), largos (horas) o muy largos (días y semanas)?</h5></li>
+              <li>  <h5>¿Es una dificultad para percibir el paso del tiempo, para medirlo o para tenerlo presente?</h5></li>
+              <li>  <h5>¿Cómo evolucionan estos problemas?</h5></li>
 
-          </ul>
-          <img class="der" src="images\Consejos\33.jpeg" alt="" />
-          <h3> Recomendaciones externas </h3>
-          <p class="intro">
-            Lo más socorrido cuando un niño con TDAH tiene dificultades con el manejo del tiempo es utilizar ayudas externas como:
-            <ul>
-              <li>  <h5>Indicarle que ya es hora de empezar </h5> </li>
-              <li> <h5> Avisarle el tiempo que tiene disponible </h5> </li>
-              <li> <h5> Utilizar agendas </li></h5>
-              <li> <h5> <a href="dinamicas.php">Horarios o calendarios.</a> </h5> </li>
-              <li> <h5> Enseñar técnicas para priorizar tareas </h5> </li>
             </ul>
+          </div>
 
-          <h3> Recomendaciones para sucesos pasados y futuros </h3>
-          <p class="intro cons">
-            Entrenar la habilidad de estimación temporal para sucesos pasados y futuros:
-            <ul>
-            <li>¿Cuánto te parece que has tardado en…?</li>
-            <li>¿Cuánto crees que tardarás en…?</li>
-          </ul>
-          </p>
-            <h3> Recomendaciones para medidas de tiempo </h3>
-          <p class="intro cons">
-            Familiarizarse con las medidas de tiempo:
-          <ul>
-            <li>Usar el reloj o el calendario para actividades en las que tenga interés.</li>
-            <li>Aprender la fecha de su cumpleaños, de las vacaciones.</li>
-            <li>Saber a qué hora se levantan, se acuestan, comen.</li>
-            <li>Mirar el menú de la semana si se quedan al comedor escolar.</li>
-            <li>Jugar a juegos en los que hay tiempo limitado para hacer las cosas.</li>
-            <li>Practicar deportes cronometrados.</li>
-          </ul>
-          </p>
-          <h3> Recomendaciones para el aula </h3>
-          <p class="intro cons">
-            Reforzar los conceptos temporales en clase de matemáticas:
-            <ul>
-            <li>Lectura de la hora</li>
-            <li>Cálculo del tiempo transcurrido entre dos momentos, equivalencias temporales (cuántos días tiene un año, cuántos minutos tienen una hora, etc.)</li>
-          </p>
 
+          <ul class="media-list">
+            <div class="animatedParent">
+              <hr>
+              <li class="media animated fadeInUpShort">
+                <div class="media-left media-middle">
+                  <img class="circle" src="images\Consejos\4.jpg" alt="icon">
+                </div>
+                <div class="media-body">
+                  <h3> Recomendaciones externas </h3>
+                  <p class="intro">
+                    Lo más socorrido cuando un niño con TDAH tiene dificultades con el manejo del tiempo es utilizar ayudas externas como:
+                    <ul>
+                      <li>  <h5>Indicarle que ya es hora de empezar </h5> </li>
+                      <li> <h5> Avisarle el tiempo que tiene disponible </h5> </li>
+                      <li> <h5> Utilizar agendas </li></h5>
+                      <li> <h5> <a href="dinamicas.php">Horarios o calendarios.</a> </h5> </li>
+                      <li> <h5> Enseñar técnicas para priorizar tareas </h5> </li>
+                    </ul>
+                </div>
+              </li>
+            </div>
+            <div class="animatedParent">
+              <hr>
+              <li class="media animated fadeInUpShort">
+                <div class="media-left media-middle">
+                  <img class="circle" src="images\Consejos\13.jpg" alt="icon">
+                </div>
+                <div class="media-body">
+                  <h3> Recomendaciones para sucesos pasados y futuros </h3>
+                  <p class="intro cons">
+                    Entrenar la habilidad de estimación temporal para sucesos pasados y futuros:
+                  <ul>
+                    <li><h5>¿Cuánto te parece que has tardado en…?</h5></li>
+                    <li><h5>¿Cuánto crees que tardarás en…?</h5></li>
+                  </ul>
+                  </p>
+                </div>
+              </li>
+            </div>
+            <div class="animatedParent">
+              <hr>
+              <li class="media animated fadeInUpShort">
+                <div class="media-left media-middle">
+                  <img class="circle" src="images\Consejos\8.jpg" alt="icon">
+                </div>
+                <div class="media-body">
+                  <h3> Recomendaciones para medidas de tiempo </h3>
+                  <p class="intro cons">
+                    Familiarizarse con las medidas de tiempo:
+                    <ul>
+                      <li><h5>Usar el reloj o el calendario para actividades en las que tenga interés.</h5></li>
+                      <li><h5>Aprender la fecha de su cumpleaños, de las vacaciones.</h5></li>
+                      <li><h5>Saber a qué hora se levantan, se acuestan, comen.</h5></li>
+                      <li><h5>Mirar el menú de la semana si se quedan al comedor escolar.</h5></li>
+                      <li><h5>Jugar a juegos en los que hay tiempo limitado para hacer las cosas.</h5></li>
+                      <li><h5>Practicar deportes cronometrados.</h5></li>
+                    </ul>
+                  </p>
+                </div>
+              </li>
+            </div>
+            <div class="animatedParent">
+              <hr>
+              <li class="media animated fadeInUpShort">
+                <div class="media-left media-middle">
+                  <img class="circle" src="images\Consejos\11.jpg" alt="icon">
+                </div>
+                <div class="media-body">
+                  <h3> Recomendaciones para el aula </h3>
+                  <p class="intro cons">
+                    Reforzar los conceptos temporales en clase de matemáticas:
+                    <ul>
+                    <li><h5>Lectura de la hora</h5></li>
+                    <li><h5>Cálculo del tiempo transcurrido entre dos momentos, equivalencias temporales (cuántos días tiene un año, cuántos minutos tienen una hora, etc.)</h5></li>
+                  </p>
+                </div>
+              </div>
+            </div>
+
+
+          </ul>
         </div>
-
       </div>
-
-
-
-
     </div>
 
 
@@ -95,6 +137,7 @@
     <script type="text/javascript" src="node_modules\bootstrap\dist\js\bootstrap.js"></script>
     <script type="text/javascript" src="node_modules\bootstrap-material-design\dist\js\material.js"></script>
     <script type="text/javascript" src="node_modules\bootstrap-material-design\dist\js\ripples.js"></script>
+    <script src='js/css3-animate-it.js'></script>
     <script type="text/javascript">
       $.material.init()
     </script>
